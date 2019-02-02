f6b51cb168da0f942e8a1a2949f7782392a36acf
matcool <26722564+matcool@users.noreply.github.com>
2019-02-02 16:45:31 -0300

    improvements

--- sketches/braillepaint.js
+++ sketches/braillepaint.js
@@ -1,87 +1,102 @@
 function brailleSketch(sketch) {
 
-	var grid = [];
-	var inverted = false;
-	var bSize = 1;
-	var cButton;
+	let grid;
+	let gw = 60;
+	let gh = 64;
+	let sx, sy;
+	let inverted = false;
+	let bSize = 1;
+	let cButton;
+	let textP;
+	let tool = 'brush';
+
 
 	sketch.setup = () => {
 		sketch.createCanvas(500, 500);
-		var genButton = sketch.createElement('a','generate');
-		var tButton = sketch.createElement('a','invert');
-		var cButton = sketch.createElement('a','clear');
-		textP = sketch.createElement('p','hello!');
-		textP.style('font-family','Iosevka Web');
-		textP.style('line-height','80%');
-		genButton.mouseClicked(genBraille);
-		genButton.class('waves-effect waves-light btn');
-		tButton.mouseClicked(toggleV);
-		tButton.class('waves-effect waves-light btn');
-		cButton.mouseClicked(resetG);
-		cButton.class('waves-effect waves-light btn');
-		//pixelDensity(1);
 		resetG();
+		sx = sketch.width / gw;
+		sy = sketch.height / gh;
+
+		let genButton = sketch.createElement('a', 'generate');
+		let tButton = sketch.createElement('a', 'invert');
+		let cButton = sketch.createElement('a', 'clear');
+		textP = sketch.createElement('p', 'hello!');
+		textP.style('font-family', 'Iosevka Web');
+		textP.style('line-height', '100%');
+		genButton.mouseClicked(genBraille);
+		tButton.mouseClicked(toggleV);
+		cButton.mouseClicked(resetG);
+		genButton.class('waves-effect waves-light btn');
+		tButton.class('waves-effect waves-light btn');
+		cButton.class('waves-effect waves-light btn');
 	}
 
 	function resetG() {
-		var s = sketch.width / 10;
-		for (var x = 0; x < s; x++) {
-			grid[x] = [];
-			for (var y = 0; y < s + 1; y++) {
-				grid[x][y] = true;
-			}
-		}
+		grid = Array(gh).fill(0).map(i => Array(gw).fill(0));
 	}
 
-	sketch.draw = () => {
-		sketch.background(100)
-		sketch.noStroke();
-		//fill(255);
-		sketch.ellipseMode(sketch.CORNER);
-		var spc = 10;
-		var gridmx = Math.floor(sketch.constrain(sketch.mouseX / (sketch.width / grid.length), 0, grid.length - 1));
-		var gridmy = Math.floor(sketch.constrain(sketch.mouseY / (sketch.width / grid.length), 0, grid.length - 1));
-		var offscreen = sketch.mouseX > sketch.width || sketch.mouseY > sketch.height;
-		for (var x = 0; x < grid.length; x++) {
-			for (var y = 0; y < grid[0].length; y++) {
-				if (!grid[x][y]) {
-					if (x == gridmx && y == gridmy) {
-						sketch.fill(0, 255, 0);
-					} else {
-						sketch.fill(255);
-					}
-					sketch.ellipse(x * spc + 2.5, y * spc + 2.5, spc / 2, spc / 2);
-				} else if (x == gridmx && y == gridmy) {
-					sketch.fill(255, 0, 0, 50);
-					sketch.ellipse(x * spc + 2.5, y * spc + 2.5, spc / 2, spc / 2);
-				}
-			}
-		}
-		if (sketch.mouseIsPressed && !offscreen) {
-			//grid[gridmx][gridmy] = false;
-			for (var xx = -bSize; xx < bSize; xx++) {
-				for (var yy = -bSize; yy < bSize; yy++) {
-					if (gridmx + xx < grid.length && gridmx + xx > 0 && gridmy + yy < grid[0].length && gridmy + yy > 0) {
-						grid[gridmx + xx][gridmy + yy] = false;
-					}
-				}
-			}
-		}
-		//rect(gridmx*spc,gridmy*spc,10,10);
-		//rect(gridmx,gridmy,10,10);
+	function mouseOob() {
+		return sketch.mouseX < 0 || sketch.mouseX > sketch.width || sketch.mouseY < 0 || sketch.mouseY > sketch.height
 	}
 
 	function toggleV() {
 		inverted = !inverted;
 	}
 
+	sketch.draw = () => {
+		sketch.background(100)
+		sketch.noStroke();
+		sketch.fill(255);
+		sketch.ellipseMode(sketch.CORNER);
+		for (let y = 0; y < gh; y++) {
+			for (let x = 0; x < gw; x++) {
+				let p = grid[y][x];
+				if (p) {
+					sketch.ellipse(x * sx, y * sy, sx, sy);
+				}
+			}
+		}
+		let gmx = Math.floor(sketch.mouseX / sx);
+		let gmy = Math.floor(sketch.mouseY / sy);
+		if (sketch.mouseIsPressed && !mouseOob()) {
+			switch (tool) {
+				case 'brush':
+					paint(gmx, gmy);
+				case 'eraser':
+					paint(gmx, gmy, true);
+			}
+		}
+	}
+
+	function paint(x, y, erase = false) {
+		let t = erase ? 1 : 0
+		if (bSize == 0) {
+			grid[y][x] = t;
+		} else {
+			for (let j = -bSize; j <= bSize; j++) {
+				for (let i = -bSize; i <= bSize; i++) {
+					if (y + j >= 0 && y + j < gh && x + i >= 0 && x + i < gw) grid[y + j][x + i] = t;
+				}
+			}
+		}
+	}
+
 	function genBraille() {
-		var finalText = '';
-		let checks = [[1,2],[1,1],[1,0],[0,2],[0,1],[0,0]];
-		for (var y = 0; y < grid[0].length; y += 3) {
-			for (var x = 0; x < grid.length; x += 2) {
-				var surround = checks.map(i => grid[x+i[0]][y+i[1]] ^ inverted ? 0 : 1).join('');
-				finalText += eval(`"\\u${(0x2800+parseInt(surround,2)).toString(16)}"`);
+		let finalText = '';
+		let checks = [
+			[1, 3],
+			[0, 3],
+			[1, 2],
+			[1, 1],
+			[1, 0],
+			[0, 2],
+			[0, 1],
+			[0, 0]
+		];
+		for (let y = 0; y < gh; y += 4) {
+			for (let x = 0; x < gw; x += 2) {
+				let tile = checks.map(i => grid[y + i[1]][x + i[0]] == inverted ? 0 : 1).join('');
+				finalText += String.fromCharCode(0x2800 + parseInt(tile, 2));
 			}
 			finalText += '<br>';
 		}
