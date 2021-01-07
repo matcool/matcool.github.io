8e545f5f42af28702f082d11ec52dee20420a4fb
matcool <26722564+matcool@users.noreply.github.com>
2021-01-07 12:45:37 -0300

    update to vite 2.0 and use compat tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { UserConfig } from 'vite';
+import type { UserConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default {
-    plugins: [],
-    assetsDir: 'assets',
+    plugins: [vue()],
+    build: {
+        assetsDir: 'assets',
+    },
 } as UserConfig;
