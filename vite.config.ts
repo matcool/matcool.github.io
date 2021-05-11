604dd4c2a81a52651c9f7de0a576d2ba76a41a58
matcool <26722564+matcool@users.noreply.github.com>
2021-05-11 14:05:07 -0300

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
     plugins: [vue()],
     build: {
         assetsDir: 'assets',
     },
-} as UserConfig;
+});
