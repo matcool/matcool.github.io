1842b8019a71a9ceb60563297b12a1ee723c824d
matcool <26722564+matcool@users.noreply.github.com>
2021-07-04 15:56:28 -0300

    will this work

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,7 @@ export default defineConfig({
         assetsDir: 'assets',
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                '404': resolve(__dirname, '404.html')
+                main: resolve(__dirname, '404.html')
             }
         }
     },
