7160f206ecdee465e97479d153694c712548250e
matcool <26722564+matcool@users.noreply.github.com>
2021-07-04 20:46:04 -0300

    hacky fix

--- src/components/GDMod.vue
+++ src/components/GDMod.vue
@@ -6,7 +6,10 @@
             <div class="">
                 <h2 class="text-3xl text-white mb-2">{{ title }}</h2>
                 <p class="text-white max-w-lg">{{ description }}</p>
-                <a :href="link" class="bg-green-400 hover:bg-green-500 active:bg-green-600 focus:outline-none mt-5 p-2 font-semibold flex cursor-pointer select-none w-min"><object :data="downloadIcon" class="mr-1" />Download</a>
+                <a
+                :href="link"
+                class="bg-green-400 hover:bg-green-500 active:bg-green-600 focus:outline-none mt-5 p-2 pr-8 font-semibold inline-flex cursor-pointer select-none w-min">
+                <img :src="downloadIcon" class="w-full mr-1" />Download</a>
             </div>
         </div>
     </div>
